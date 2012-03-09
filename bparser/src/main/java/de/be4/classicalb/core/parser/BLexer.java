220d0b16a83e0fc2d4dd60b3903189ce780117d2
Jens Bendisposto <jens@bendisposto.de>
2012-03-09 13:50:13 +0100

    fixed some more warnings

--- bparser/src/main/java/de/be4/classicalb/core/parser/BLexer.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/BLexer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PushbackReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import de.be4.classicalb.core.parser.exceptions.BLexerException;
 import de.be4.classicalb.core.parser.lexer.Lexer;
