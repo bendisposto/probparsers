050b261bbc87e11bbf9364ce78f76185a36b4ac4
Sebastian Krings <sebastian@krin.gs>
2012-04-23 20:16:23 +0200

    adding a first (failing) test to understand gradle and the build/test system

---
+++ bparser/src/test/java/de/be4/classicalb/core/parser/analysis/pragma/machines/PragmaMachines.java
@@ -0,0 +1,13 @@
+package de.be4.classicalb.core.parser.analysis.pragma.machines;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PragmaMachines {
+
+	@Test
+	public void testSimple() {
+		assertEquals(1, 2);
+	}
+}
