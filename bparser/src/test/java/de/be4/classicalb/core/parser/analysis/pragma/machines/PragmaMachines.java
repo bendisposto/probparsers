9a95e5051ea54110fbd2bf947aa0c418df7a6412
Sebastian Krings <sebastian@krin.gs>
2012-04-23 20:32:56 +0200

    reworked test to a working one - using to string for now

--- bparser/src/test/java/de/be4/classicalb/core/parser/analysis/pragma/machines/PragmaMachines.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/analysis/pragma/machines/PragmaMachines.java
@@ -1,13 +1,34 @@
 package de.be4.classicalb.core.parser.analysis.pragma.machines;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
+import de.be4.classicalb.core.parser.BParser;
+import de.be4.classicalb.core.parser.analysis.pragma.internal.UnknownPragma;
+import de.be4.classicalb.core.parser.exceptions.BException;
+
 public class PragmaMachines {
 
+	private static final String PATH = "src/test/resources/pragmas/";
+
+	private File machine;
+
 	@Test
-	public void testSimple() {
-		assertEquals(1, 2);
+	public void testBusPragma() throws IOException, BException {
+		machine = new File(PATH + "BusPragma.mch");
+		final BParser parser = new BParser(machine.getName());
+		parser.parseFile(machine, false);
+
+		assertEquals(14, parser.getPragmas().size());
+
+		assertTrue(parser.getPragmas().get(0) instanceof UnknownPragma);
+		assertEquals(
+				"Pragma 'goto hell' (2,17)-(2,33). Right after 'Start/na', inside 'AExpressionDefinitionDefinition' and before 'AAddExpression/AIdentifierExpression'",
+				parser.getPragmas().get(0).toString());
 	}
 }
