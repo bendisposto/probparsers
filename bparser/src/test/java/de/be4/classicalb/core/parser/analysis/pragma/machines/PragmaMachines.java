9e307e40ab365132818484b054481289e20590db
Sebastian Krings <sebastian@krin.gs>
2012-04-24 09:38:12 +0200

    reworked first test - using prolog output now

--- bparser/src/test/java/de/be4/classicalb/core/parser/analysis/pragma/machines/PragmaMachines.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/analysis/pragma/machines/PragmaMachines.java
@@ -6,29 +6,44 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.analysis.pragma.internal.UnknownPragma;
+import de.be4.classicalb.core.parser.analysis.prolog.NodeIdAssignment;
 import de.be4.classicalb.core.parser.exceptions.BException;
+import de.be4.classicalb.core.parser.node.Start;
+import de.prob.prolog.output.PrologTermStringOutput;
 
 public class PragmaMachines {
 
 	private static final String PATH = "src/test/resources/pragmas/";
+	private PrologTermStringOutput out;
+	private NodeIdAssignment ids;
 
 	private File machine;
+
+	@Before
+	public void before() {
+		out = new PrologTermStringOutput();
+		ids = new NodeIdAssignment();
+	}
 
 	@Test
 	public void testBusPragma() throws IOException, BException {
 		machine = new File(PATH + "BusPragma.mch");
+
 		final BParser parser = new BParser(machine.getName());
-		parser.parseFile(machine, false);
+		Start start = parser.parseFile(machine, false);
+		start.apply(ids);
 
 		assertEquals(14, parser.getPragmas().size());
 
 		assertTrue(parser.getPragmas().get(0) instanceof UnknownPragma);
+		parser.getPragmas().get(0).printProlog(out, ids);
 		assertEquals(
-				"Pragma 'goto hell' (2,17)-(2,33). Right after 'Start/na', inside 'AExpressionDefinitionDefinition' and before 'AAddExpression/AIdentifierExpression'",
-				parser.getPragmas().get(0).toString());
+				"global_pragma(goto,[hell],[],-1,2,17,2,33,[start,start,eof,start,eof])",
+				out.toString());
 	}
 }
