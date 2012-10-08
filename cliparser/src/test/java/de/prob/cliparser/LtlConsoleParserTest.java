361e74130ec76953c6c1faa6975527aa0239fcce
Jens Bendisposto <jens@bendisposto.de>
2012-10-08 13:29:26 +0200

    added a high-level test for LTL (PARSERLIB-11)

--- cliparser/src/test/java/de/prob/cliparser/LtlConsoleParserTest.java
+++ cliparser/src/test/java/de/prob/cliparser/LtlConsoleParserTest.java
@@ -9,15 +9,26 @@ import org.junit.Test;
 public class LtlConsoleParserTest {
 
 	@Test(timeout=1000)
-	public void testLtlCommandline_parserlib11() throws Exception {
-		File temp = File.createTempFile("parserlib_11", ".ltl");
+	public void testLtlCommandline_parserlib11_ctl() throws Exception {
+		File temp = File.createTempFile("parserlib_11_ctl_", ".ltl");
 		BufferedWriter out = new BufferedWriter(new FileWriter(temp));
 		out.write("AG {taken={}");
 		out.close();
 		String absolutePath = temp.getAbsolutePath();
-		System.out.println(absolutePath);
 		LtlConsoleParser
 				.main(new String[] { "-ctl", "-lang", "B", absolutePath });
+	}
+
+	
+	@Test(timeout=1000)
+	public void testLtlCommandline_parserlib11_ltl() throws Exception {
+		File temp = File.createTempFile("parserlib_11_ltl", ".ltl");
+		BufferedWriter out = new BufferedWriter(new FileWriter(temp));
+		out.write("G {taken={}");
+		out.close();
+		String absolutePath = temp.getAbsolutePath();
+		LtlConsoleParser
+		.main(new String[] { "-ltl", "-lang", "B", absolutePath });
 	}
 
 }
