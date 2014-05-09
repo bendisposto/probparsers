d9cde30a5e96843505c8e3ee9c1206811e6c45e0
Jens Bendisposto <jens@bendisposto.de>
2014-05-09 11:16:59 +0200

    PARSERLIB-28 regression test for 536

---
+++ bparser/src/test/java/de/be4/classicalb/core/parser/DropHaskellRegressionTests.java
@@ -0,0 +1,30 @@
+package de.be4.classicalb.core.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import de.be4.classicalb.core.parser.exceptions.BException;
+import de.be4.classicalb.core.parser.node.Start;
+
+public class DropHaskellRegressionTests {
+
+	@Test
+	public void test() throws BException, IOException {
+		String name = "src/test/resources/parsable/InfiniteParityFunction.mch";
+		BParser parser = new BParser(name);
+		parser.parseFile(new File(name), false);
+
+		String code = "not(finite({x|x>2}))";
+		BParser parser2 = new BParser(name);
+		parser2.getDefinitions().addAll(parser.getDefinitions());
+		Start parse = parser2.parse(BParser.FORMULA_PREFIX + " " + code, false,
+				parser.getContentProvider());
+		assertNotNull(parse);
+
+	}
+
+}
