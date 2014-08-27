10f00c31f2cf9f34ad987490384d3542ade2298f
Sebastian Krings <sebastian@krin.gs>
2014-08-27 12:08:43 +0200

    escape unicode character

--- eventbstruct/src/test/java/de/be4/eventb/parser/StructureTest.java
+++ eventbstruct/src/test/java/de/be4/eventb/parser/StructureTest.java
@@ -73,7 +73,7 @@ public class StructureTest extends AbstractTest {
 
 	@Test
 	public void testUnicodeIdentifiers1() throws Exception {
-		parseInput("context UnicodeIdentifiers1 constants Über æ mäh end",
+		parseInput("context UnicodeIdentifiers1 constants Über \u00E6 mäh end",
 				false);
 	}
 
