7e47fa742ebb743a20e151f1d2eecd9262f2e772
Sebastian Krings <sebastian@krin.gs>
2014-09-18 10:13:45 +0200

    unit tests for order of definitions

---
+++ bparser/src/test/java/de/be4/classicalb/core/parser/definitions/DefinitionsOrderTest.java
@@ -0,0 +1,37 @@
+package de.be4.classicalb.core.parser.definitions;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import de.be4.classicalb.core.parser.BParser;
+import de.be4.classicalb.core.parser.exceptions.BException;
+import de.be4.classicalb.core.parser.node.Start;
+
+public class DefinitionsOrderTest {
+
+	private static final String PATH = "src/test/resources/definitions/";
+
+	private File machine;
+
+	@Test
+	public void testLinearOrder() throws IOException, BException {
+		machine = new File(PATH + "DefinitionsOccurInLinearOrder.mch");
+
+		final BParser parser = new BParser(machine.getName());
+		Start start = parser.parseFile(machine, false);
+		assertNotNull(start);
+	}
+
+	@Test
+	public void testReordered() throws IOException, BException {
+		machine = new File(PATH + "DefinitionsOccurReordered.mch");
+
+		final BParser parser = new BParser(machine.getName());
+		Start start = parser.parseFile(machine, false);
+		assertNotNull(start);
+	}
+}
