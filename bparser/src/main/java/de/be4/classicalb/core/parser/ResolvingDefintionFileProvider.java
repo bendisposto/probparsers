61107f3f57f458850697acbc113e5f3c8f6f0860
David Schneider <david.schneider@bivab.de>
2015-01-15 13:15:14 +0100

    Add a definiton file provider that extends the caching defintion file provider.
    This provider tries, in case the file is not found in the location relative to the current machine, to resolve it using the PROBPATH environment variable. PROBPATH is a “:” separated list of directories.

---
+++ bparser/src/main/java/de/be4/classicalb/core/parser/ResolvingDefintionFileProvider.java
@@ -0,0 +1,41 @@
+package de.be4.classicalb.core.parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class ResolvingDefintionFileProvider extends CachingDefinitionFileProvider {
+    public ResolvingDefintionFileProvider(final File parentFile) {
+        super(parentFile);
+    }
+
+    public File getFile(final String filename) {
+        File f = super.getFile(filename);
+        if(f.exists() && f.isFile()) {
+            return f;
+        }
+        try {
+            return resolve(filename);
+        } catch (IOException e) {
+            return f;
+        }
+    }
+
+    private File resolve(String fileName) throws FileNotFoundException {
+        String probPath = System.getenv("PROBPATH");
+        if(probPath == null) {
+            probPath = ".";
+        }
+
+        for (String s : probPath.split(":")) {
+            Path path = FileSystems.getDefault().getPath(s, fileName);
+            java.io.File f = new java.io.File(path.toString());
+            if(f.exists() && f.isFile()) {
+                return f;
+            }
+        }
+        throw new FileNotFoundException();
+    }
+}
