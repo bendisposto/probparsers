2194c042bc8b7504564bf1d54f1361ee62ce32fd
David Schneider <david.schneider@bivab.de>
2015-03-12 16:44:55 +0100

    toString Implementation and tests for Record

--- translator/src/main/java/de/prob/translator/types/Record.java
+++ translator/src/main/java/de/prob/translator/types/Record.java
@@ -1,7 +1,7 @@
 package de.prob.translator.types;
 
-import java.lang.String;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -146,6 +146,10 @@ public class Record implements BObject, Map<java.lang.String, BObject> {
 	 *             from being stored in this map
 	 */
 	public BObject put(java.lang.String key, BObject value) {
+		throw new UnsupportedOperationException();
+	}
+
+	public BObject putAt(java.lang.String key, Object value) {
 		throw new UnsupportedOperationException();
 	}
 
@@ -317,8 +321,23 @@ public class Record implements BObject, Map<java.lang.String, BObject> {
 		this.map = map;
 	}
 
-	public String toString() {
-		return "rec(" + map + ')';
+	public java.lang.String toString() {
+		Iterator<java.util.Map.Entry<java.lang.String, BObject>> it = this.entrySet().iterator();
+		StringBuffer sb = new StringBuffer();
+		sb.append("rec(");
+		while(it.hasNext()) {
+			Map.Entry<java.lang.String, BObject> entry = (java.util.Map.Entry<java.lang.String, BObject>) it.next();
+			sb.append(entry.getKey());
+			sb.append(": ");
+			sb.append(entry.getValue());
+			if(it.hasNext()) {
+				sb.append(", ");
+			}
+		}
+		sb.append(")");
+		return sb.toString();
+	}
+
 	public static Map<java.lang.String, BObject> newStorage() {
 		return new LinkedHashMap<java.lang.String, BObject>();
 	}
---
+++ translator/src/main/java/de/prob/translator/types/RecordTest.groovy
@@ -0,0 +1,99 @@
+package de.prob.translator.types;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import de.prob.translator.Translator
+
+
+class RecordTest {
+
+	def rec1
+	def rec2
+	def rec3
+	def one
+	@Before
+	public void setUp() throws Exception {
+		this.rec1 = Translator.translate("rec(a:1, b:2)")
+		this.rec2 = Translator.translate("rec(a:1, b:2)")
+		this.rec3 = Translator.translate("rec(a:1, b:3)")
+		this.one = Translator.translate("1")
+	}
+
+	@Test
+	public void testSize() {
+		assert rec1.size() == 2
+	}
+
+	@Test
+	public void testIsEmpty() {
+		assertFalse rec1.isEmpty()
+	}
+
+	@Test
+	public void testContainsKey() {
+		assert rec1.containsKey("a")
+		assertFalse rec1.containsKey("c")
+	}
+
+	@Test
+	public void testContainsValue() {
+		assert rec1.containsValue(one)
+		assertFalse rec1.containsValue(1)
+		assertFalse rec1.containsValue(3)
+	}
+
+	@Test
+	public void testGet() {
+		assert rec1.get('a') == one
+	}
+
+	@Test
+	public void testGetAt() {
+		assert rec1['a'] == one
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testPutAt() {
+		rec1['a'] = 5
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testPut() {
+		rec1.put('a', one)
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testRemove() {
+		rec1.remove(one)
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testPutAll() {
+		rec1.putAll(['c': one])
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testClear() {
+		rec1.clear()
+	}
+
+	@Test
+	public void testKeySet() {
+		rec1.keySet().size() == 2
+		rec1.keySet().containsAll(['a', 'b'])
+	}
+
+	@Test
+	public void testEquals() {
+		assert rec1.equals(rec2)
+		assertFalse rec1.equals(rec3)
+	}
+
+	@Test
+	public void testToString() {
+		def str = rec1.toString();
+		assert str == "rec(a: 1, b: 2)"
+	}
+}
