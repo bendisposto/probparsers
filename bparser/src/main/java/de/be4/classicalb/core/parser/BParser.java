5884a654fb13f4a1544fd131e60b057996303412
David Schneider <david.schneider@bivab.de>
2017-01-31 16:57:13 +0100

    Removed duplicated readFile method

--- bparser/src/main/java/de/be4/classicalb/core/parser/BParser.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/BParser.java
@@ -173,37 +173,8 @@ public class BParser {
 		if (verbose) {
 			DebugPrinter.println("Parsing file '" + machineFile.getCanonicalPath() + "'");
 		}
-		String content = readFile(machineFile);
+		String content = Utils.readFile(machineFile);
 		return parse(content, verbose, contentProvider);
-	}
-
-	public final String readFile(final File machine) throws FileNotFoundException, IOException {
-		final InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(machine));
-
-		final StringBuilder builder = new StringBuilder();
-		final char[] buffer = new char[1024];
-		int read;
-		while ((read = inputStreamReader.read(buffer)) >= 0) {
-			builder.append(String.valueOf(buffer, 0, read));
-		}
-		String content = builder.toString();
-
-		inputStreamReader.close();
-
-		// remove utf-8 byte order mark
-		// replaceAll \uFEFF did not work for some reason
-		// apparently, unix like systems report a single character with the code
-		// below
-		if (!content.isEmpty() && Character.codePointAt(content, 0) == 65279) {
-			content = content.substring(1);
-		}
-		// while windows splits it up into three characters with the codes below
-		if (!content.isEmpty() && Character.codePointAt(content, 0) == 239 && Character.codePointAt(content, 1) == 187
-				&& Character.codePointAt(content, 2) == 191) {
-			content = content.substring(3);
-		}
-
-		return content.replaceAll("\r\n", "\n");
 	}
 
 	/**
