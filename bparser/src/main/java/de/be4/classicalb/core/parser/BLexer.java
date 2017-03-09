10775b31c95a79b938da50bce1cccda6b514b8bd
hansen <dominik_hansen@web.de>
2017-03-09 11:32:41 +0100

    support escape symbol ‘\’ in strings

--- bparser/src/main/java/de/be4/classicalb/core/parser/BLexer.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/BLexer.java
@@ -19,6 +19,7 @@ public class BLexer extends Lexer {
 
 	private static Map<Class<? extends Token>, Map<Class<? extends Token>, String>> invalid = new HashMap<Class<? extends Token>, Map<Class<? extends Token>, String>>();
 	private static Set<Class<? extends Token>> clauseTokenClasses = new HashSet<>();
+	private static Map<Character, Character> stringReplacements = new HashMap<>();
 
 	private static void addInvalid(Class<? extends Token> f, Class<? extends Token> s, String message) {
 		Map<Class<? extends Token>, String> secs = invalid.get(f);
@@ -58,7 +59,16 @@ public class BLexer extends Lexer {
 			addInvalid(TConjunction.class, clauseTokenClass, "& " + clauseName + " is not allowed.");
 		}
 
+		// replacements in strings '\' + ..
+		// e.g. '\' + 'n' is replaced by '\n'
+		stringReplacements.put('"', '"');
+		stringReplacements.put('\'', '\'');
+		stringReplacements.put('n', '\n');
+		stringReplacements.put('r', '\r');
+		stringReplacements.put('t', '\t');
+		stringReplacements.put('\\', '\\');
 	}
+	
 	private ParseOptions parseOptions = null;
 
 	private Token comment = null;
@@ -152,18 +162,29 @@ public class BLexer extends Lexer {
 			if (literal.startsWith("'''")) {
 				/// '''foo'''
 				literal = literal.substring(3, literal.length() - 3);
-				// delete all backslashes of escaped quotes e.g. a\'b -> a'b
-				literal = literal.replace("\\'", "'");
-				token.setText(literal);
 			} else {
 				/// "foo"
 				literal = literal.substring(1, literal.length() - 1);
-				// delete all backslashes of escaped quotes e.g. a\"b -> a"b
-				literal = literal.replace("\\\"", "\"");
-				token.setText(literal);
 			}
 
+			boolean backslashFound = false;
+			StringBuffer buffer = new StringBuffer();
+			for (int i = 0; i < literal.length(); i++) {
+				char c = literal.charAt(i);
+				if (backslashFound && stringReplacements.containsKey(c)) {
+					buffer.setLength(buffer.length() - 1);
+					buffer.append(stringReplacements.get(c));
+					backslashFound = false;
+					continue;
+				}
+				if (c == '\\') {
+					backslashFound = true;
+				}
+				buffer.append(c);
+			}
+			token.setText(buffer.toString());
 		}
+		
 		if (token instanceof THexLiteral) {
 			final String literal = token.getText().substring(2);
 			int value = Integer.valueOf(literal, 16);
