68f36d5e94aa56f17a68a82eadce948cb3469a43
dohan <dohan001@hhu.de>
2017-08-25 11:13:30 +0200

    add STRING_FORMAT tests

--- bparser/src/test/java/de/be4/classicalb/core/parser/rules/StringFormatOperatorTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/rules/StringFormatOperatorTest.java
@@ -8,7 +8,32 @@ import org.junit.Test;
 public class StringFormatOperatorTest {
 
 	@Test
-	public void testStringFormatConcat() {
+	public void testStringFormat() {
+		final String testMachine = "RULES_MACHINE Test PROPERTIES STRING_FORMAT(\" ~w ~w \", 1, 2) = \" 1 2 \" END";
+		String result = getRulesMachineAsPrologTerm(testMachine);
+		System.out.println(result);
+		assertFalse(result.contains("exception"));
+	}
+
+	@Test
+	public void testStringFormatWrongNumberOfArguments() {
+		final String testMachine = "RULES_MACHINE Test PROPERTIES STRING_FORMAT(\" ~w ~w \", 1) = \" 1 2 \" END";
+		String result = getRulesMachineAsPrologTerm(testMachine);
+		System.out.println(result);
+		assertTrue(result.contains(
+				"parse_exception(pos(1,31,'UnknownFile'),'The number of arguments (1) does not match the number of placeholders (2) in the string.')"));
+	}
+
+	@Test
+	public void testStringFormatWithConcat() {
+		final String testMachine = "RULES_MACHINE Test PROPERTIES STRING_FORMAT(\" ~w \" ^  \"~w \", 1, 2) = \" 1 2 \" END";
+		String result = getRulesMachineAsPrologTerm(testMachine);
+		System.out.println(result);
+		assertFalse(result.contains("exception"));
+	}
+
+	@Test
+	public void testStringFormatWithConcatWrongNumberOfArguments() {
 		final String testMachine = "RULES_MACHINE Test PROPERTIES STRING_FORMAT(\" ~w \" ^  \"~w \", 1) = \" 1 2 \" END";
 		String result = getRulesMachineAsPrologTerm(testMachine);
 		System.out.println(result);
@@ -30,15 +55,6 @@ public class StringFormatOperatorTest {
 		String result = getRulesMachineAsPrologTerm(testMachine);
 		System.out.println(result);
 		assertFalse(result.contains("exception"));
-	}
-
-	@Test
-	public void testStringFormat() {
-		final String testMachine = "RULES_MACHINE Test PROPERTIES STRING_FORMAT(\" ~w ~w \", 1) = \" 1 2 \" END";
-		String result = getRulesMachineAsPrologTerm(testMachine);
-		System.out.println(result);
-		assertTrue(result.contains(
-				"parse_exception(pos(1,31,'UnknownFile'),'The number of arguments (1) does not match the number of placeholders (2) in the string.')"));
 	}
 
 }
