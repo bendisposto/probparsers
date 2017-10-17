feb7bf0bb56ad939d604228014b18f56479e6e4d
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2017-10-17 11:57:39 +0200

    add ast caching

--- cliparser/src/test/java/de/prob/cliparser/FregeCspParserTest.java
+++ cliparser/src/test/java/de/prob/cliparser/FregeCspParserTest.java
@@ -12,8 +12,8 @@ public class FregeCspParserTest {
 
     @Test
     public void singleQuoteInParseErrorIsEscaped() {
-        String lotsOfCommands = "diff(NonTock,{|enter,leave|}\n../../prob_examples/public_examples/CSP/mydemos/demo/crossing.csp";
-        BufferedReader in = fromString(lotsOfCommands);
+        String commands = "diff(NonTock,{|enter,leave|}\n../../prob_examples/public_examples/CSP/mydemos/demo/crossing.csp";
+        BufferedReader in = fromString(commands);
         String parseResult = FregeCspParser.translateCspExpressionToProlog(in);
         String expected = "frege_facts('ParseErrorException: ParseError \"\\nunexpected ''(''\\nexpecting end of module\" (Token (TokenId 2320) (AlexPn 14480 384 28) 4 L_Ident \"diff\") (AlexPn 14480 384 28)').\n";
         assertEquals(expected, parseResult);
@@ -21,5 +21,15 @@ public class FregeCspParserTest {
 
     private static BufferedReader fromString(String string) {
         return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(string.getBytes())));
+    }
+
+    @Test
+    public void testAstCaching() {
+        String commands = "diff(NonTock,{|enter,leave|}\n../../prob_examples/public_examples/CSP/mydemos/demo/crossing.csp";
+        BufferedReader in1 = fromString(commands);
+        String parseResult1 = FregeCspParser.translateCspExpressionToProlog(in1);
+        BufferedReader in2 = fromString(commands);
+        String parseResult2 = FregeCspParser.translateCspExpressionToProlog(in2);
+        assertEquals(parseResult1, parseResult2);
     }
 }
