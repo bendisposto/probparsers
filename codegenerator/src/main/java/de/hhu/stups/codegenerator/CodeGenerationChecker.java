1f4d2ffbaae107781db93cdbe4c26c8aa69d1f67
Fabian Vu <Fabian.Vu@hhu.de>
2018-06-12 15:26:14 +0200

    Add CodeGenerationChecker

---
+++ codegenerator/src/main/java/de/hhu/stups/codegenerator/CodeGenerationChecker.java
@@ -0,0 +1,174 @@
+package de.hhu.stups.codegenerator;
+
+import de.prob.parser.ast.nodes.MachineNode;
+import de.prob.parser.ast.nodes.expression.ExpressionOperatorNode;
+import de.prob.parser.ast.nodes.expression.IdentifierExprNode;
+import de.prob.parser.ast.nodes.expression.NumberNode;
+import de.prob.parser.ast.nodes.expression.QuantifiedExpressionNode;
+import de.prob.parser.ast.nodes.expression.SetComprehensionNode;
+import de.prob.parser.ast.nodes.ltl.LTLBPredicateNode;
+import de.prob.parser.ast.nodes.ltl.LTLInfixOperatorNode;
+import de.prob.parser.ast.nodes.ltl.LTLKeywordNode;
+import de.prob.parser.ast.nodes.ltl.LTLPrefixOperatorNode;
+import de.prob.parser.ast.nodes.predicate.CastPredicateExpressionNode;
+import de.prob.parser.ast.nodes.predicate.IdentifierPredicateNode;
+import de.prob.parser.ast.nodes.predicate.PredicateOperatorNode;
+import de.prob.parser.ast.nodes.predicate.PredicateOperatorWithExprArgsNode;
+import de.prob.parser.ast.nodes.predicate.QuantifiedPredicateNode;
+import de.prob.parser.ast.nodes.substitution.AnySubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.AssignSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.BecomesElementOfSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.BecomesSuchThatSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.ConditionSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.IfOrSelectSubstitutionsNode;
+import de.prob.parser.ast.nodes.substitution.ListSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.SkipSubstitutionNode;
+import de.prob.parser.ast.visitors.AbstractVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fabian on 12.06.18.
+ */
+public class CodeGenerationChecker implements AbstractVisitor<Void, Void> {
+
+    private List<String> errors;
+
+    private MachineNode node;
+
+    public CodeGenerationChecker(MachineNode node) {
+        this.errors = new ArrayList<>();
+        this.node = node;
+    }
+
+    public void check() {
+        node.getOperations().forEach(operation -> visitSubstitutionNode(operation.getSubstitution(), null));
+        if(node.getProperties() != null) {
+            visitPredicateNode(node.getProperties(), null);
+        }
+        if(node.getInitialisation() != null) {
+            visitSubstitutionNode(node.getInitialisation(), null);
+        }
+    }
+
+    @Override
+    public Void visitLTLPrefixOperatorNode(LTLPrefixOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLKeywordNode(LTLKeywordNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLInfixOperatorNode(LTLInfixOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLBPredicateNode(LTLBPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIdentifierPredicateNode(IdentifierPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitPredicateOperatorNode(PredicateOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitPredicateOperatorWithExprArgs(PredicateOperatorWithExprArgsNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitQuantifiedPredicateNode(QuantifiedPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitExprOperatorNode(ExpressionOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIdentifierExprNode(IdentifierExprNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitCastPredicateExpressionNode(CastPredicateExpressionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitNumberNode(NumberNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitQuantifiedExpressionNode(QuantifiedExpressionNode node, Void expected) {
+        errors.add("Quantified expressions cannot be generated");
+        return null;
+    }
+    @Override
+    public Void visitSetComprehensionNode(SetComprehensionNode node, Void expected) {
+        return null;
+    }
+
+
+    @Override
+    public Void visitListSubstitutionNode(ListSubstitutionNode node, Void expected) {
+        if(node.getOperator() == ListSubstitutionNode.ListOperator.Parallel) {
+            errors.add("Parallel substitution cannot be generated");
+        }
+        return null;
+    }
+
+
+    @Override
+    public Void visitIfOrSelectSubstitutionsNode(IfOrSelectSubstitutionsNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitAssignSubstitutionNode(AssignSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitSkipSubstitutionNode(SkipSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitConditionSubstitutionNode(ConditionSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitAnySubstitution(AnySubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitBecomesElementOfSubstitutionNode(BecomesElementOfSubstitutionNode node, Void expected) {
+        errors.add("Becomes element of cannot be generated");
+        return null;
+    }
+
+    @Override
+    public Void visitBecomesSuchThatSubstitutionNode(BecomesSuchThatSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
